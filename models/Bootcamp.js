68d56e2c1b425147a6209f1b1f3975c9eabbfd33
manouch <manoujon@gmail.com>
2026-05-03 13:19:37 -0500

    Populate, Virtuals & Cascade Delete

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -132,6 +132,23 @@ BootcampSchema.pre('save', async function () {
   this.address = undefined;
 });
 
+// Cascade delete courses when a bootcamp is deleted
+// BootcampSchema.pre('remove', async function (next) {
+//   console.log(`Courses being removed from bootcamp ${this._id}`);
+//   await this.model('Course').deleteMany({ bootcamp: this._id });
+//   next();
+// });
+
+BootcampSchema.pre(
+  'deleteOne',
+  { document: true, query: false },
+  async function (next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    // next();
+  }
+);
+
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
